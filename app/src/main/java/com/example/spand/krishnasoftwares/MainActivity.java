da4faa1ed594d59b483fe2179bcc4a8a95e6f310
Konstantinos Stoumpos <stoombydoo@hotmail.com>
2019-01-17 10:25:42 +0200

    added settings activity

--- app/src/main/java/com/example/spand/krishnasoftwares/MainActivity.java
+++ app/src/main/java/com/example/spand/krishnasoftwares/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.spand.krishnasoftwares;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -66,6 +67,8 @@ public class MainActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+            Intent myIntent = new Intent(MainActivity.this, SettingsActivity.class);
+            MainActivity.this.startActivity(myIntent);
             return true;
         }
 
