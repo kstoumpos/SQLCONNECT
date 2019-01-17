53aa5f91c86af1c36cabd82df85f78e1475ef985
Konstantinos Stoumpos <stoombydoo@hotmail.com>
2019-01-17 09:37:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/spand/krishnasoftwares/ConnectionClass.java
@@ -0,0 +1,40 @@
+package com.example.spand.krishnasoftwares;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionClass {
+    String ip = "192.168.1.14";
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "smartBonus_lite";
+    String un = "sa";
+    String password = "123456";
+
+    private static String port = "57169";
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        Connection conn = null;
+        String ConnURL = null;
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            Class.forName(classs);
+            ConnURL = "jdbc:jtds:sqlserver://" + ip  + ":57169" + ";"
+                    + "databaseName=" + db + ";user=" + un + ";password="
+                    + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+            Log.e("OK", "ok");
+        } catch (SQLException e) {
+            Log.e("ERROR", e.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERROR", e.getMessage());
+        }
+        return conn;
+    }
+}
